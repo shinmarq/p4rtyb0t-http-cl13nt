cae25897f4beaba4e0b657ec2273c519f2c73bf9
Oscar Mendoza <oscar@smartwave.ph>
2016-11-27 15:03:52 +0800

    Auth

--- modules/auth.js
+++ modules/auth.js
@@ -2,8 +2,27 @@ var request = require('request'),
 	MapResponse = require('../response_body_mapper/MapResponse'),
 	constants = require('../constants');
 
-// const URL = constants.BASE_PATH + constants.API_PATH + "/login";
+const URL = constants.BASE_PATH + constants.API_PATH + "/token";
 
-module.exports.login = function(params, callback) {
-	callback(params);
+module.exports.getToken = function(params, callback) {
+	var options = {
+		headers: {
+			Authorization:'Basic b2ZmaWNpYWxBcGlDbGllbnQ6QzBGRkVF'
+		},
+		method: 'post',
+		body: params,
+		url: URL,
+		json: true
+	};
+
+	request(options, function (error, response, body) {
+		// console.log(error, response.statusCode, body.d);
+		if(error == null && response.statusCode == constants.CREATED) {
+			var mapResponse = new MapResponse(body);
+			var newBody = mapResponse.mapData();
+			callback(null, response, newBody);
+		} else {
+			callback(body, response, null);
+		}		
+	});
 };
